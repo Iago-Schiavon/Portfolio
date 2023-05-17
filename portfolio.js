f477eb42cbf86e14a671ddc0b48586ef1524672b
Iago Schiavon <iago.ctrlplay@gmail.com>
2023-05-17 10:37:40 -0300

    Modal

--- portfolio.js
+++ portfolio.js
@@ -13,10 +13,10 @@ function myFunction() {
 }
 
 // Get the modal
-var modal = document.getElementById('myModal');
+var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
+var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
@@ -29,6 +29,6 @@ img.onclick = function(){
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
+span.onclick = function() {
   modal.style.display = "none";
 }
