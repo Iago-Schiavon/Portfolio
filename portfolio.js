a2c0ef62355f4dfdf1f86685560d865906eaebc6
Iago Schiavon <iago.ctrlplay@gmail.com>
2023-08-18 13:02:06 -0300

    b

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,5 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+/*var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg");
